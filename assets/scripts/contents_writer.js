8464cd6c430693731ab5b2d4c279d1575eeb33fd
AO2324-00 <ao.2324.00@gmail.com>
2022-06-02 21:40:09 +0900

    調整

--- assets/scripts/contents_writer.js
+++ assets/scripts/contents_writer.js
@@ -20,13 +20,13 @@ class ContentsWriter {
         if(name in this.#contents) {
             this.#element.innerHTML = "";
             for(let child of this.#contents[name]) this.#element.appendChild(child.cloneNode(true));
-            window.scroll({top: 0, behavior: 'smooth'});
+            window.scroll({top: 0, behavior: 'instant'});
             return;
         }
         const content = await this.getContent(name);
         this.#element.innerHTML = content;
         if(name == "menu") await setupMenu(document.getElementById("menu_box"));
         if(this.#stored_elements.includes(name)) this.#contents[name] = this.#element.cloneNode(true).children;
-        window.scroll({top: 0, behavior: 'smooth'});
+        window.scroll({top: 0, behavior: 'instant'});
     }
 }
